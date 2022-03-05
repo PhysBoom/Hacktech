64be9f0b7b6c08a84f83fa5ed116e27cc22868a3
PhysBoom <mc_gold05@yahoo.com>
2022-03-04 22:51:23 -0800

    Added super basic navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './index.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-primary">
-      <span className="text-blue-500">Hello World</span>
+    <div className="w-screen h-screen bg-white">
+      <Navbar />
     </div>
   );
 }
