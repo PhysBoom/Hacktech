2e7d36d7d4a891ee0d9b20bc93474f53237ffaa2
dhruvkulkarni1 <DhruvK0003@gmail.com>
2022-03-05 10:38:30 -0800

    gauth stuff

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, url_for, redirect
 from utility.firebase_pushable_object import FirebasePushableObject
 import os
 import waitress
 from dotenv import dotenv_values
+import json
+import sqlite3
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+
 
 ENVIRONMENT = dotenv_values(".env")["FLASK_ENV"]
 
